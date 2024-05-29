db874c6dcbbbc8a522cf110244c09bdd9766860f
KGH1113 <gangguhyeon1113@gmail.com>
2024-05-29 22:55:04 +0900

    frontend design changed

--- index.js
+++ index.js
@@ -6,18 +6,14 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-// 클라이언트 연결 관리
 const clients = new Set();
 
 wss.on("connection", (ws) => {
-  console.log("클라이언트가 연결되었습니다.");
+  console.log("Client has been connected.");
 
-  // 새로운 클라이언트를 연결 목록에 추가
   clients.add(ws);
 
-  // 클라이언트로부터 메시지 수신
   ws.on("message", (message) => {
-    // 연결된 모든 클라이언트에게 메시지 전송
     console.log(message.toString());
     for (const client of clients) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
@@ -26,10 +22,8 @@ wss.on("connection", (ws) => {
     }
   });
 
-  // 클라이언트 연결 종료
   ws.on("close", () => {
-    console.log("클라이언트가 연결을 종료했습니다.");
-    // 연결 종료된 클라이언트를 연결 목록에서 제거
+    console.log("One of the client has been disconnected");
     clients.delete(ws);
   });
 });
@@ -43,5 +37,5 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`서버가 ${PORT}번 포트에서 실행 중입니다.`);
+  console.log(`Server is running on port: ${PORT}`);
 });
